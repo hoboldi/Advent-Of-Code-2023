300a0d3800fa1fe29f43fd47e9c3d30f87974081
ge45sim <ge45sim@mytum.de>
2023-12-12 00:39:41 +0100

    Day 11.1

---
+++ Day11/Day11.2.cpp
@@ -0,0 +1,182 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <fstream>
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <stack>
+#include <queue>
+
+enum Weight {
+    One = 1,
+    Double = 2
+};
+
+
+struct Vertex {
+    uint x,y;
+    int id;
+    Weight w;
+    uint distance;
+
+    Vertex(uint x, uint y) {
+        this->x = x;
+        this->y = y;
+        this->id = -1;
+        this->w = One;
+    }
+
+    Vertex(uint x, uint y, int id) {
+        this->x = x;
+        this->y = y;
+        this->id = id;
+        this->w = One;
+    }
+};
+
+typedef std::vector<std::vector<Vertex>> Universe;
+
+void setDistanceMax(Universe& universe) {
+    for(std::vector<Vertex>& line: universe) {
+        for(Vertex& v: line) {
+            v.distance = (std::numeric_limits<uint>::max())/2;
+        }
+    }
+}
+
+std::vector<std::vector<uint>> distances;
+
+void BFS(Universe& universe, uint x, uint y) {
+    std::queue<std::pair<uint,uint>> queue;
+    queue.push(std::make_pair(x,y));
+
+    uint startid = universe.at(x).at(y).id;
+
+    while(!queue.empty()) {
+        std::pair<uint,uint> coords = queue.front();
+        queue.pop();
+        Vertex thisVertex = universe.at(coords.first).at(coords.second);
+
+
+        if (thisVertex.id != -1 && thisVertex.id != startid) {
+            distances.at(startid).at(thisVertex.id) = thisVertex.distance;
+        }
+        if (coords.first > 0 && thisVertex.distance + thisVertex.w < universe.at(coords.first - 1).at(coords.second).distance) {
+            universe.at(coords.first - 1).at(coords.second).distance = thisVertex.distance + thisVertex.w;
+            queue.push(std::make_pair(coords.first - 1,coords.second));
+        }
+
+        if (coords.second > 0 && thisVertex.distance + thisVertex.w < universe.at(coords.first).at(coords.second - 1).distance) {
+            universe.at(coords.first).at(coords.second - 1).distance = thisVertex.distance + thisVertex.w;
+            queue.push(std::make_pair(coords.first,coords.second - 1));
+        }
+
+        if (coords.first < universe.size() - 1 && thisVertex.distance + thisVertex.w < universe.at(coords.first + 1).at(coords.second).distance) {
+            universe.at(coords.first + 1).at(coords.second).distance = thisVertex.distance + thisVertex.w;
+            queue.push(std::make_pair(coords.first + 1,coords.second));
+        }
+
+        if (coords.second < universe.at(0).size() - 1 && thisVertex.distance + thisVertex.w < universe.at(coords.first).at(coords.second + 1).distance) {
+            universe.at(coords.first).at(coords.second + 1).distance = thisVertex.distance + thisVertex.w;
+            queue.push(std::make_pair(coords.first,coords.second + 1));
+        }
+    }
+}
+
+//Could be optimized
+void getDistances(Universe universe) {
+    for(size_t i = 0; i < universe.size(); i++) {
+        for(size_t j = 0; j < universe.at(i).size(); j++) {
+            Vertex& v = universe.at(i).at(j);
+            if(v.id != -1) {
+                setDistanceMax(universe);
+                v.distance = 0;
+                BFS(universe,v.x,v.y);
+            }
+        }
+    }
+}
+
+int main() {
+    std::string inputName = "../../Day11/Day11.txt";
+    std::ifstream inputFile (inputName);
+    if (!inputFile)
+        throw std::runtime_error("Could not open file " + std::string(inputName));
+
+    std::vector <std::string> input(0);
+
+    while (true) {
+        std::string line;
+        getline(inputFile, line);
+        input.push_back(line);
+
+        if(inputFile.eof())
+            break;
+    }
+
+    uint nextId{0};
+
+    Universe universe(input.size());
+    for(size_t i = 0; i < input.size(); i++) {
+        for(size_t j = 0; j < input.at(i).size(); j++) {
+            if(input.at(i).at(j) == '#') {
+                universe.at(i).emplace_back(i,j,nextId++);
+            } else {
+                universe.at(i).emplace_back(i,j);
+            }
+        }
+    }
+
+    for(size_t i = 0; i < universe.size(); i++) {
+        bool allSpace = true;
+
+        for(size_t j = 0; j < universe.at(i).size(); j++) {
+            if(universe.at(i).at(j).id != -1) {
+                allSpace = false;
+            }
+        }
+
+        if(allSpace) {
+            for (size_t j = 0; j < universe.at(i).size(); j++) {
+                universe.at(i).at(j).w = Double;
+            }
+        }
+    }
+
+    uint galaxies = 0;
+
+    for(size_t i = 0; i < universe.at(0).size(); i++) {
+        bool allSpace = true;
+
+        for(size_t j = 0; j < universe.size(); j++) {
+            if(universe.at(j).at(i).id != -1) {
+                allSpace = false;
+                galaxies++;
+            }
+        }
+
+        if(allSpace) {
+            for (size_t j = 0; j < universe.size(); j++) {
+                universe.at(j).at(i).w = Double;
+            }
+        }
+    }
+
+    std::vector<uint> d(galaxies);
+    distances.resize(galaxies,d);
+    getDistances(universe);
+
+    uint sum = 0;
+    for(std::vector<uint> line: distances) {
+        for(uint x: line) {
+            sum += x;
+        }
+    }
+    sum /= 2;
+
+    assert(sum == 9965032);
+    std::cout << sum << std::endl;
+}
