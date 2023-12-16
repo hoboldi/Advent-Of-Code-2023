a3ea13e96ed156c3de4ca5eb7ba2845ee6a5a05f
ge45sim <ge45sim@mytum.de>
2023-12-16 23:04:49 +0100

    Day 13.1

---
+++ Day13/Day13.1.cpp
@@ -0,0 +1,120 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <fstream>
+#include <algorithm>
+#include <cassert>
+
+
+enum Pattern {
+  Ash,
+  Rock
+};
+
+typedef std::vector<std::vector<Pattern>> Patterns;
+
+bool checkEqualityOfVector(std::vector<Pattern>& a, std::vector<Pattern>& b) {
+  if(a.size() != b.size())
+    return false;
+
+  for(std::size_t i = 0; i < a.size(); i++) {
+      if(a[i] != b[i])
+        return false;
+  }
+  return true;
+}
+
+
+uint getColomns(Patterns p){
+  int indexL;
+  int indexR;
+
+  for(std::size_t i = 1; i < p.size(); i++) {
+    if(checkEqualityOfVector(p.at(i - 1),p.at(i))) {
+        indexL = (int)i - 1;
+        indexR = (int)i;
+
+        bool isReflection = true;
+        //start check
+        while(indexL >= 0 && indexR < p.size()) {
+          if(checkEqualityOfVector(p.at(indexL), p.at(indexR))) {
+            indexL--;
+            indexR++;
+          } else {
+            isReflection = false;
+            break;
+          }
+        }
+
+        if(isReflection)
+          return i;
+    }
+  }
+
+  return 0;
+}
+
+
+uint solvePatterns(Patterns p) {
+  uint returnValue;
+  returnValue = 100 * getColomns(p);
+  if(returnValue != 0)
+    return returnValue;
+  //rotate p
+  Patterns newPattern(p[0].size());
+  for(std::size_t i = 0; i < newPattern.size(); i++) {
+    newPattern[i].resize(p.size());
+  }
+
+  for(std::size_t i = 0; i < p.size(); i++) {
+    for(std::size_t j = 0; j < p[i].size(); j++) {
+      newPattern[j][i] = p[i][j];
+    }
+  }
+
+  returnValue += getColomns(newPattern);
+  return returnValue;
+}
+
+
+int main() {
+    std::string inputName = "../../Day13/Day13.txt";
+    std::ifstream inputFile (inputName);
+    if (!inputFile)
+        throw std::runtime_error("Could not open file " + std::string(inputName));
+
+    std::vector <std::string> input(0);
+
+    while (true) {
+        std::string line;
+        getline(inputFile, line);
+        input.push_back(line);
+
+        if(inputFile.eof())
+            break;
+    }
+
+    Patterns p(0);
+    uint result = 0;
+
+    for(std::size_t i = 0; i < input.size(); i++) {
+      std::vector<Pattern> line(input.at(i).size());
+      for(std::size_t j = 0; j < input.at(i).size(); j++) {
+        line.at(j) = input.at(i).at(j) == '.' ? Ash : Rock;
+      }
+
+      if(input.at(i).size() == 0) {
+        result += solvePatterns(p);
+        p.resize(0);
+      } else {
+        p.push_back(line);
+      }
+    }
+    result += solvePatterns(p);
+
+
+    //assert(sum == 9965032);
+    std::cout << result << std::endl;
+}
---
+++ Day13/Day13.2.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <fstream>
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <stack>
+#include <queue>
+
+uint solveLine(const std::string &line) {
+  std::istringstream iss(line);
+  std::vector<int> returnVector(0);
+
+  std::string springs;
+  std::getline(iss, springs, ' ');
+  std::string blueprint;
+  std::getline(iss, blueprint, ' ');
+
+
+
+
+}
+
+
+
+int main() {
+  std::string inputName = "../../Day12/Day12.txt";
+  std::ifstream inputFile (inputName);
+  if (!inputFile)
+    throw std::runtime_error("Could not open file " + std::string(inputName));
+
+  std::vector <std::string> input(0);
+
+  while (true) {
+    std::string line;
+    getline(inputFile, line);
+    input.push_back(line);
+
+    if(inputFile.eof())
+      break;
+  }
+
+  for(std::string line: input) {
+    solveLine(line);
+  }
+
+
+
+}
