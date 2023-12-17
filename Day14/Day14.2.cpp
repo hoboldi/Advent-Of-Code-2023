44a20dca1276c0ab18d80877d9fbd9b4985e9d00
ge45sim <ge45sim@mytum.de>
2023-12-17 02:31:59 +0100

    Day 14.2

--- Day14/Day14.2.cpp
+++ Day14/Day14.2.cpp
@@ -15,25 +15,81 @@ enum Element {
 
 typedef std::vector<std::vector<Element>> Map;
 
-//sort not necessary could be done in linear time for example with two pointer approach
-uint tiltNorth (Map& map) {
+void rotateMatrix(Map &matrix) {
+  if (matrix.empty() || matrix.size() != matrix[0].size()) {
+    std::cout << "Invalid matrix format!" << std::endl;
+    return;
+  }
+
+  int n = matrix.size(); // Assuming square matrix
+
+  // Create a new matrix to store the rotated elements
+  Map rotatedMatrix(n, std::vector<Element>(n));
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      rotatedMatrix[j][n - 1 - i] = matrix[i][j];
+    }
+  }
+
+  // Copy the rotated matrix back to the original matrix
+  matrix = rotatedMatrix;
+}
+
+void printMatrix(const Map &matrix) {
+  for (const auto &row : matrix) {
+    for (const auto &element : row) {
+      switch (element) {
+      case 0:
+        std::cout << "O";
+        break;
+      case 1:
+        std::cout << ".";
+        break;
+      case 2:
+        std::cout << "#";
+        break;
+      }
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+//brute force :( could be improved with cycle detection
+uint runCycle (Map map) {
   uint result = 0;
+
+  rotateMatrix(map);
+  rotateMatrix(map);
+
+  for(std::size_t k = 0; k < 4 * 1000000000; k++) {
+
+    std::cout << 4 * 1000000000 - k <<  std::endl;
+
+    rotateMatrix(map);
+
+    for(std::size_t i = 0; i < map.size(); i++)
+    {
+      uint last = 0;
+      for(std::size_t j = 0; j < map[i].size(); j++) {
+        if(map[i][j] == CubeRock) {
+          std::sort(map[i].begin() + last,map[i].begin() + (uint) j);
+          last = j + 1;
+        }
+      }
+      std::sort(map[i].begin() + last,map[i].end());
+    }
+  }
+  rotateMatrix(map);
+  rotateMatrix(map);
+  printMatrix(map);
 
   for(std::size_t i = 0; i < map.size(); i++)
   {
-    uint last = 0;
-    for(std::size_t j = 0; j < map[i].size(); j++) {
-      if(map[i][j] == CubeRock) {
-        std::sort(map[i].begin() + last,map[i].begin() + (uint) j);
-        last = j + 1;
-      }
-    }
-
-    std::sort(map[i].begin() + last,map[i].end());
-
     for(std::size_t j = 0; j < map[i].size(); j++) {
       if(map[i][j] == RoundRock)
-        result += map[i].size() - j;
+        result += map[j].size() - i;
     }
   }
 
@@ -59,34 +115,32 @@ int main() {
       break;
   }
 
-  Map map(input[0].size());
+  Map map(input.size());
   for(std::vector<Element>& line: map ) {
-    line.resize(input.size());
+    line.resize(input[0].size());
   }
 
   for(std::size_t i = 0; i < input.size(); i++) {
     for(std::size_t j = 0; j < input[0].size(); j++) {
-
       switch (input[i][j]) {
       case '.':
-        map[j][i] = Space;
+        map[i][j] = Space;
         break;
       case '#':
-        map[j][i] = CubeRock;
+        map[i][j] = CubeRock;
         break;
       case 'O':
-        map[j][i] = RoundRock;
+        map[i][j] = RoundRock;
         break;
       }
-
     }
   }
 
-  uint result = tiltNorth(map);
+  uint result = runCycle(map);
 
 
 
 
-  assert(result == 110407);
+  //assert(result == 110407);
   std::cout << result << std::endl;
 }
